3e1505abb8fee24049359f34d4649a07169f1c95
dmfernandesivp <92241913+dmfernandesivp@users.noreply.github.com>
2023-04-17 10:21:53 +0530

    Add files via upload

---
+++ create-projects.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+green="\033[1;32m"
+reset="\033[m"
+
+echo "creating directory"
+mkdir server
+cd server
+
+echo -e "${green}Creating solution and projects${reset}"
+dotnet new sln
+dotnet new webapi -n API
+dotnet new classlib -n Application
+dotnet new classlib -n Domain
+dotnet new classlib -n Infrastructure
+
+echo -e "${green}Adding projects to the solution${reset}"
+
+dotnet sln add API/API.csproj
+dotnet sln add Application/Application.csproj
+dotnet sln add Infrastructure/Infrastructure.csproj
+
+echo -e "${green}Adding projects to the solution${reset}"
+cd API
+dotnet add reference ../Application/Application.csproj
+cd ../Application
+dotnet add reference ../Domain/Domain.csproj
+dotnet add reference ../Infrastructure/Infrastructure.csproj
+cd ../Infrastructure
+dotnet add reference ../Domain/Domain.csproj
+cd ..
+
+
+echo -e "${green}Executing dotnet restore${reset}"
+dotnet restore
+
+echo -e "${green}Finished!${reset}"
